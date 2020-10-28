1ccbc1dab5f2426d4574eb34a82bebefbcc4e808
thesnoopy <devel@mailsmart.de>
2020-10-28 10:14:09 +0100

    Initial commit

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+	"amadeus adapter settings": {
+		"en": "Adapter settings for amadeus",
+		"de": "Adaptereinstellungen für amadeus",
+		"ru": "Настройки адаптера для amadeus",
+		"pt": "Configurações do adaptador para amadeus",
+		"nl": "Adapterinstellingen voor amadeus",
+		"fr": "Paramètres d'adaptateur pour amadeus",
+		"it": "Impostazioni dell'adattatore per amadeus",
+		"es": "Ajustes del adaptador para amadeus",
+		"pl": "Ustawienia adaptera dla amadeus",
+		"zh-cn": "amadeus的适配器设置"
+	},
+	"option1": {
+		"en": "option1",
+		"de": "option1",
+		"ru": "option1",
+		"pt": "option1",
+		"nl": "option1",
+		"fr": "option1",
+		"it": "option1",
+		"es": "option1",
+		"pl": "option1",
+		"zh-cn": "option1"
+	},
+	"option2": {
+		"en": "option2",
+		"de": "option2",
+		"ru": "option2",
+		"pt": "option2",
+		"nl": "option2",
+		"fr": "option2",
+		"it": "option2",
+		"es": "option2",
+		"pl": "option2",
+		"zh-cn": "option2"
+	}
+};
